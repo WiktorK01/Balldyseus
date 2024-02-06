8e6bf7e104f78182f4e0823353930558eae93b52
WiktorK01 <145882626+WiktorK01@users.noreply.github.com>
2024-02-06 13:56:06 -0500

    huhyh

--- Assets/C# Scripts/SceneLoader.cs
+++ Assets/C# Scripts/SceneLoader.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadCurrentScene()
     {
-        SceneManager.LoadScene(sceneName);
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        int nextSceneIndex = currentSceneIndex + 1;
+        
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load");
+        }
+    }
+
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
     }
 }
